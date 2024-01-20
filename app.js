96f14b5d5c790d8c70cad9fa9d845751763e59b2
VHumFF <108907994+VHumFF@users.noreply.github.com>
2024-01-20 23:29:17 +0800

    established database

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const session = require('express-session');
+app.use(session({
+  secret: 'fwdd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 var livereload = require("livereload");
 var connectLiveReload = require("connect-livereload");
+const bodyParser = require('body-parser');
+const db = require('./models/db');
+
 
 
 const liveReloadServer = livereload.createServer();
@@ -18,6 +30,10 @@ liveReloadServer.server.once("connection", () => {
     liveReloadServer.refresh("/");
   }, 100);
 });
+
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,7 +52,44 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
+app.get('/homepage', (req, res) => {
+  if (!req.session.user) {
+  // User is not logged in, redirect to login page
+    res.redirect('/');
+  } else {
+  // User is logged in, render the dashboard
+    res.render('homepage', { user_name: req.session.user_name });
+  }
+});
 
+app.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if(err) {
+    // Handle error
+      console.log(err);
+      res.send('Error occurred during logout');
+    } else {
+  // Redirect to login page after successful logout
+      res.redirect('/');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  let sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  let query = db.query(sql, [req.body.username, req.body.password],
+  (err, result) => {
+    if (err) throw err;
+    if (result.length > 0) {
+  // Login successful, set session and redirect to dashboard
+      req.session.user = result[0];
+      req.session.user_name = result[0].username;
+      res.redirect('/homepage');
+    } else {
+// Login failed, respond with error message
+      res.send('Login failed');
+    }
+});});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/db.js
@@ -0,0 +1,12 @@
+// database.js
+const db = require('../config/database');
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+module.exports = db;
