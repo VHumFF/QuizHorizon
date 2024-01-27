7ffa7f5350190e96987a614d999ef88fde602507
VHumFF <108907994+VHumFF@users.noreply.github.com>
2024-01-27 21:21:53 +0800

    mobile navibar added

--- public/javascripts/navibar.js
+++ public/javascripts/navibar.js
@@ -1,8 +1,33 @@
 jQuery(document).ready(function ($) {
   $('#um_btn').click(function (e) {
-    e.preventDefault();
-
-    // Toggle the 'show' class for the sub-element
-    $(this).next('.um-show').toggleClass('show');
+    $('#um-show').toggleClass('show');
   });
 });
+
+jQuery(document).ready(function ($) {
+  $('#sm_btn').click(function (e) {
+    $('#sm-show').toggleClass('show');
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $('#mobile_um_btn').click(function (e) {
+    $('#mobile_um-show').toggleClass('show');
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $('#mobile_sm_btn').click(function (e) {
+    $('#mobile_sm-show').toggleClass('show');
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $('.hamburger').click(function (e) {
+    $('.hamburger').toggleClass('is-active');
+    $('.mobile-nav').toggleClass('is-active');
+  });
+
+});
+
+
